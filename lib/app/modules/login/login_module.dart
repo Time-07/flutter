ddfa8dbba4fbf5b44f2d7fbd14453397542952f5
Arthur <arturkaizer@gmail.com>
2022-04-04 00:45:39 -0300

    imports of login_module fixed

--- lib/app/modules/login/login_module.dart
+++ lib/app/modules/login/login_module.dart
@@ -2,6 +2,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:trans_app/app/modules/login/presentation/login_controller.dart';
 import 'package:trans_app/app/modules/login/presentation/login_page.dart';
 
+import 'presentation/login_controller.dart';
+
 class LoginModule extends Module{
   @override
   List<Bind> get binds => [
